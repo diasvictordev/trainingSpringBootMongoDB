0524b83062dfd7345b311c8c5e3cc0b3c0290754
diasvictordev <victor.dias@aluno.ueg.br>
2023-06-29 14:55:56 -0300

    add exception e findbyid

--- src/main/java/com/mongodbtraining/mongotraining/services/UserService.java
+++ src/main/java/com/mongodbtraining/mongotraining/services/UserService.java
@@ -2,10 +2,12 @@ package com.mongodbtraining.mongotraining.services;
 
 import com.mongodbtraining.mongotraining.domain.User;
 import com.mongodbtraining.mongotraining.repository.UserRepository;
+import com.mongodbtraining.mongotraining.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -14,5 +16,11 @@ public class UserService {
     private UserRepository repo;
     public List<User> findAll(){
         return repo.findAll();
+    }
+
+    public User findById(String id){
+        Optional <User> obj = repo.findById(id);
+        return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado!"));
+
     }
 }
